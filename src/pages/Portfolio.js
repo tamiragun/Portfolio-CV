47862d2324925d614cdb8a8705e3101d46fcf93b
Tamira <t.gunzburg@gmail.com>
2021-08-24 10:14:44 +0200

    Add data for portfolio projects

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -25,8 +25,57 @@ const portfolioProjects = [
     prod_url: null,
   },
   {
+    title: "Git search",
+    stack: ["Node", "Express", "JavaScript", "React", "CSS"],
+    description:
+      "This capstone project for the Node/JS module at HyperionDev, I built a search function that retrieves a user from the Github and/or Gitlab API, and returns their basic profile information. You can also drill down into their top repositories and see their latest commits.",
+    github_url: "https://github.com/tamiragun/hangman",
+    prod_url: "https://tamiragun-hangman.herokuapp.com/",
+  },
+  {
     title: "Wedding bells",
-    stack: ["HTML", "SASS", "JavaScript", "jQuery"],
+    stack: ["PHP", "MySQL", "Wordpress"],
+    description:
+      "A paired project where my brother made the front end of his wedding website on Wordpress and I implemented some back-end and database functionality to enable guests to reserve gifts on the registry.",
+    github_url:
+      "https://github.com/tamiragun/wedding-register-wordpress-plugin",
+    prod_url: "http://www.buggenburg.be/",
+  },
+  {
+    title: "Ricca furniture",
+    stack: ["HTML", "CSS", "JavaScript", "Netlify"],
+    description:
+      "As a part of the FEDSA mentoring programme, I built a homepage for a fictitious furniture company, based on a Figma design and some interactive feature requests.",
+    github_url: "https://github.com/tamiragun/fedsa-project-1",
+    prod_url: "https://fedsa-project-1.netlify.app/",
+  },
+  {
+    title: "Hangman",
+    stack: ["JavaScript", "React", "CSS", "Heroku"],
+    description:
+      "Play the famous word guessing game against the computer with this single page application.",
+    github_url: "https://github.com/tamiragun/hangman",
+    prod_url: "https://tamiragun-hangman.herokuapp.com/",
+  },
+  {
+    title: "Cool tech",
+    stack: ["PHP", "Laravel", "SQL Server"],
+    description:
+      "This capstone project for the PHP module at HyperionDev had me build out a basic blog for the fictitious company 'Cool Tech'. It has navigation, search functionality, and loads of Latin...",
+    github_url: "https://github.com/tamiragun/capstone-laravel-blog",
+    prod_url: null,
+  },
+  {
+    title: "Monte Hall",
+    stack: ["JavaScript", "React", "CSS", "Netflify"],
+    description:
+      "Test this famous statistics riddle for yourself, by opening doors and winning prizes on my React app.",
+    github_url: "https://github.com/tamiragun/monty-hall",
+    prod_url: "https://monte-hall.netlify.app/",
+  },
+  {
+    title: "Wedding patchwork",
+    stack: ["HTML", "SCSS", "Sass", "JavaScript", "jQuery"],
     description:
       "I'm getting married! For the capstone project concluding the front-end portion of the course at HyperionDev, I decided to have a go at a website I would have otherwise had built by someone else! Check out the website or my GitHub repository. You're invited to collaborate, although perhaps not to the wedding!",
     github_url: "https://github.com/tamiragun/capstone-html-css-js-website",
